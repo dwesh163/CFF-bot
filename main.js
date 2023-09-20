b8d90fec6756df03feb3528c3b08623fe0348fc6
dwesh163 <ehouarn.duriaux@protonmail.com>
2023-09-20 14:08:38 +0200

    Add a json file

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ const { Telegraf } = require('telegraf')
 
 require('dotenv').config()
 
+const dataJson = require('data.json');
+const data = JSON.parse(dataJson);
 
-
-
+console.log(data.admin);
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-
-
 
 async function telegram() {
 
