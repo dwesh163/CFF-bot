3cd61b2d14d60fbca51579212ca3b7d80a2b0d9a
dwesh163 <ehouarn.duriaux@protonmail.com>
2023-09-25 10:50:36 +0200

    Add the first user's system

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
-const { Telegraf } = require('telegraf')
-
 require('dotenv').config()
 
-const dataJson = require('data.json');
-const data = JSON.parse(dataJson);
+const { Telegraf } = require('telegraf')
+const fs = require('fs')
+const prompt = require('prompt-sync')();
 
-console.log(data.admin);
+const filePath = './data.json';
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
+const adminID = prompt('Enter a Admin ID: ');
+let JSONObject = {}
+JSONObject[adminID] = {admin: 3}
+
+fs.writeFileSync(filePath, JSON.stringify(JSONObject, null, 3));
+
+function verifyAccount(ctx) {
+    let isAccount = new Boolean()
+    if (ctx.message.chat.id in JSON.parse(fs.readFileSync(filePath))){
+        isAccount = true
+    }
+    else{
+        isAccount = false
+    }
+
+    return isAccount
+}
 
 async function telegram() {
 
     bot.command('start', async (ctx) => {
-        ctx.reply("Hello i'm a bot...")
+        console.log(verifyAccount(ctx))
+        if (verifyAccount(ctx)){
+            ctx.reply("Hello i'm a bot...")
+        }
+                
     })
-    
+
     bot.launch()
 
-
 }
-
 
 telegram();
